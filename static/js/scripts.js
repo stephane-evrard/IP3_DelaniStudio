3b02bf87f5c35b119230881eb2ab0c2921918f2d
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-06-19 15:04:40 +0300

    portfolio hover

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -38,6 +38,61 @@ $(document).ready(function () {
     });
     // END OF PRODUCT ACCORDION
 
+    $(".portfolio1Title").hide();
+    $(".portfolio1").hover(function (e) {
+        $(".portfolio1Title").show();
+    }, function () {
+        $(".portfolio1Title").hide();
+    });
+
+    $(".portfolio2Title").hide();
+    $(".portfolio2").hover(function (e) {
+        $(".portfolio2Title").show();
+    }, function () {
+        $(".portfolio2Title").hide();
+    });
+
+    $(".portfolio3Title").hide();
+    $(".portfolio3").hover(function (e) {
+        $(".portfolio3Title").show();
+    }, function () {
+        $(".portfolio3Title").hide();
+    });
+
+    $(".portfolio4Title").hide();
+    $(".portfolio4").hover(function (e) {
+        $(".portfolio4Title").show();
+    }, function () {
+        $(".portfolio4Title").hide();
+    });
+
+    $(".portfolio5Title").hide();
+    $(".portfolio5").hover(function (e) {
+        $(".portfolio5Title").show();
+    }, function () {
+        $(".portfolio5Title").hide();
+    });
+
+    $(".portfolio6Title").hide();
+    $(".portfolio6").hover(function (e) {
+        $(".portfolio6Title").show();
+    }, function () {
+        $(".portfolio6Title").hide();
+    });
+
+    $(".portfolio7Title").hide();
+    $(".portfolio7").hover(function (e) {
+        $(".portfolio7Title").show();
+    }, function () {
+        $(".portfolio7Title").hide();
+    });
+
+    $(".portfolio8Title").hide();
+    $(".portfolio8").hover(function (e) {
+        $(".portfolio8Title").show();
+    }, function () {
+        $(".portfolio8Title").hide();
+    });
 
 
 
